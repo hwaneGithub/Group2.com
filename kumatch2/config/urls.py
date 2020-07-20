645300adf6e121c689fbf24bd398e725b6f5c644
JunyongBak <junyong3013@gmail.com>
2020-07-20 22:04:19 +0900

    연습

--- kumatch2/config/urls.py
+++ kumatch2/config/urls.py
@@ -1,4 +1,4 @@
-"""config URL Configuration
+"""config URL Configuration fgfgffgfg
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
